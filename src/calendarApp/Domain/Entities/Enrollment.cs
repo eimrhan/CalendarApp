bafb9532b5585244503fa332424f4c13e91a1986
Emirhan Çelebi <emirhan@celebi.dev>
2024-06-07 20:25:35 +0300

    added entities and connected sqlserver

---
+++ src/calendarApp/Domain/Entities/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Enrollment : Entity<Guid>
+{
+    public Guid UserId { get; set; }
+    public EnrollmentType Type { get; set; }
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
--- src/calendarApp/Domain/Entities/User.cs
+++ src/calendarApp/Domain/Entities/User.cs
@@ -1,9 +1,14 @@
-﻿namespace Domain.Entities;
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Domain.Entities;
 
 public class User : NArchitecture.Core.Security.Entities.User<Guid>
 {
+    public string UserName { get; set; }
     public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; } = default!;
     public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = default!;
     public virtual ICollection<OtpAuthenticator> OtpAuthenticators { get; set; } = default!;
     public virtual ICollection<EmailAuthenticator> EmailAuthenticators { get; set; } = default!;
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = null!;
+
 }
